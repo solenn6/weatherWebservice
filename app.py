445d2b24042a7ac251d4dfda48d1bde3000af0fa
solenn <vosport46@gmail.com>
2021-05-05 09:40:33 +0200

    ajout fichier .env

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import web
 import json
-import cleAPI
+# import cleAPI
 import requests
+import dotenv
+import os
 
-key = cleAPI.API_key
+dotenv.load_dotenv(dotenv.find_dotenv())
+API_Key = os.environ.get('API_KEY')
+DB_Host = os.environ.get('DB_HOST')
+DB_User = os.environ.get('DB_USER')
+DB_Password = os.environ.get('DB_PASSWORD')
+DB_Name = os.environ.get('DB_NAME')
+DB_Port = os.environ.get('DB_PORT')
+
+# key = cleAPI.API_key
 
 urls = (
     '/([0-9]{5}),([A-Za-z]{2})', 'Weather',
@@ -15,7 +25,7 @@ app = web.application(urls, globals())
 class Weather:
     def GET(self, codePostal, countryCode):
         web.headers = {"Content_Type": "application/json"}
-        base_url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + codePostal + ',' + countryCode + '&appid=' + key
+        base_url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + codePostal + ',' + countryCode + '&appid=' + API_Key
         data_weather = requests.get(base_url).json()
         temp_actuelle = data_weather.get('main').get('temp')
         temp_min = data_weather.get('main').get('temp_min')
